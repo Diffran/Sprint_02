0e06fbab920114a01ca744506c05160a8bedfaec
Diana <dianafrancitorraferran@gmail.com>
2024-04-22 22:49:16 +0200

    queries mongo fins la 21, falta Wil

---
+++ Sprint_02/Tasca_04/queries.mongodb.js
@@ -0,0 +1,25 @@
+use ('exercici1_Restaurant');
+db.Restaurant.find();
+db.Restaurant.find({},{restaurant_id:1, name:1, borough:1,cuisine:1});
+db.Restaurant.find({},{restaurant_id:1, name:1, borough:1,cuisine:1,_id:0});
+db.Restaurant.find({},{restaurant_id:1, name:1, borough:1,cuisine:1,_id:0, "address.zipcode":1});
+db.Restaurant.find({borough:'Bronx'});
+db.Restaurant.find({borough: 'Bronx'}).limit(5);
+db.Restaurant.find({borough: 'Bronx'}).skip(5).limit(5);
+db.Restaurant.find({"grades.score":{$gt:90}});
+db.Restaurant.find({grades:{$elemMatch: {"score":{$gt:80, $lt:100}}}});
+db.Restaurant.find({"address.coord.1":{$lt:-95.754168}});
+db.Restaurant.find({$and : [{cuisine:{$nin:['American', 'American ']}},{"grades.score":{$gt:70}},{"address.coord.0":{$lt:-65.754168}}]});
+db.Restaurant.find({cuisine:{$nin:['American', 'American ']}},{"grades.score":{$gt:70}},{"address.coord.0":{$lt:-65.754168}});
+db.Restaurant.find({cuisine:{$nin:['American', 'American ']},"grades.grade":"A","borough":"Brooklyn"}).sort({cuisine:-1});
+db.Restaurant.find({name:{$regex : "^Wil"}},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.Restaurant.find({name:{$regex : "ces$"}},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.Restaurant.find({name:{$regex : "Reg"}},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.Restaurant.find({borough:"Bronx","cuisine":{$in:["American","Chinese", "American "]}});
+db.Restaurant.find({borough:{$in:["Staten Island", "Queens", "Bronx","Brooklyn"]}},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.Restaurant.find({borough:{$nin:["Staten Island", "Queens", "Bronx","Brooklyn"]}},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.Restaurant.find({"grades.score":{$lt:10}},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0, "grades.score":1});
+db.Restaurant.find({name: {$regex: "Fish"}, cuisine:{$nin:["American ","Chinese"]}, },{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+//db.Restaurant.find();
+
+
